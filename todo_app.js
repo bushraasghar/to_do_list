e9955666e8001ef76b0c634e476a98c58b23fc68
Bushra Asghar <159794215+bushraasghar@users.noreply.github.com>
2024-04-01 20:00:15 +0500

    Add files via upload

---
+++ todo_app.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "todo",
+            message: "Whats your plan todo?"
+        },
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Do you want add more in your to do?"
+        }
+    ]);
+    const { todo, addMore } = answers;
+    loop = addMore;
+    if (todo) {
+        todos.push(todo);
+    }
+    else {
+        console.log("kindly add valid input");
+    }
+}
+if (todos.length > 0) {
+    console.log("your to do list is:");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No todos found");
+}
